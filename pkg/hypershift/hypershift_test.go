8241b176baab7e5650a8983f7248bbfd47d779ad
Simone Tiraboschi <stirabos@redhat.com>
2024-07-19 18:49:10 +0200

    Add unit test
    Add a simple unit test
    
    Signed-off-by: Simone Tiraboschi <stirabos@redhat.com>

--- pkg/hypershift/hypershift_test.go
+++ pkg/hypershift/hypershift_test.go
@@ -2,6 +2,7 @@ package hypershift
 
 import (
 	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/yaml"
@@ -87,6 +88,108 @@ spec:
     - cidr: "2001::/16"
     apiServer:
       port: 2040
+`,
+		},
+		{
+			name: "Picks up toleration",
+			expectedOutput: &HostedControlPlane{
+				ClusterID:                    "31df7fa9-b1a7-4a66-98ef-c6920bf213d8",
+				ControllerAvailabilityPolicy: "HighlyAvailable",
+				NodeSelector: map[string]string{
+					"kubernetes.io/os": "linux",
+				},
+				Tolerations: []corev1.Toleration{
+					{
+						Key:               "node.kubernetes.io/not-ready",
+						Operator:          "Exists",
+						Value:             "",
+						Effect:            "NoExecute",
+						TolerationSeconds: nil,
+					},
+					{
+						Key:               "node.kubernetes.io/unreachable",
+						Operator:          "Exists",
+						Value:             "",
+						Effect:            "NoExecute",
+						TolerationSeconds: nil,
+					},
+					{
+						Key:               "node.kubernetes.io/memory-pressure",
+						Operator:          "Exists",
+						Value:             "",
+						Effect:            "NoSchedule",
+						TolerationSeconds: nil,
+					},
+					{
+						Key:               "key1",
+						Operator:          "Equal",
+						Value:             "value1",
+						Effect:            "NoSchedule",
+						TolerationSeconds: nil,
+					},
+					{
+						Key:               "key1",
+						Operator:          "Exists",
+						Value:             "",
+						Effect:            "NoSchedule",
+						TolerationSeconds: nil,
+					},
+				},
+				AdvertiseAddress: "172.20.0.1",
+				AdvertisePort:    6443,
+				PriorityClass:    "",
+			},
+			inputUnstructuredContent: `
+apiVersion: hypershift.openshift.io/v1beta1
+kind: HostedControlPlane
+spec:
+  autoscaling: {}
+  clusterID: 31df7fa9-b1a7-4a66-98ef-c6920bf213d8
+  controllerAvailabilityPolicy: HighlyAvailable
+  dns:
+    baseDomain: tf71faa489656c98b18e2-a383e1dc466c308d41a756a1a66c2b6a-c000.us-south.satellite.test.appdomain.cloud
+  infrastructureAvailabilityPolicy: HighlyAvailable
+  nodeSelector:
+    kubernetes.io/os: linux
+  networking:
+    clusterNetwork:
+    - cidr: 10.132.0.0/14
+    networkType: OVNKubernetes
+    serviceNetwork:
+    - cidr: 172.31.0.0/16
+  olmCatalogPlacement: management
+  platform:
+    kubevirt:
+      baseDomainPassthrough: true
+      generateID: tgw7vsjfjm
+    type: KubeVirt
+  tolerations:
+  - effect: NoExecute
+    key: node.kubernetes.io/not-ready
+    operator: Exists
+    tolerationSeconds: 300
+  - effect: NoExecute
+    key: node.kubernetes.io/unreachable
+    operator: Exists
+    tolerationSeconds: 300
+  - effect: NoSchedule
+    key: node.kubernetes.io/memory-pressure
+    operator: Exists
+  - key: "key1"
+    operator: "Equal"
+    value: "value1"
+    effect: "NoSchedule"
+  - key: "key1"
+    operator: "Exists"
+    effect: "NoSchedule"
+status:
+  conditions:
+  - lastTransitionTime: "2024-07-17T05:55:02Z"
+    message: Configuration passes validation
+    observedGeneration: 1
+    reason: AsExpected
+    status: "True"
+    type: ValidHostedControlPlaneConfiguration
 `,
 		},
 	}
